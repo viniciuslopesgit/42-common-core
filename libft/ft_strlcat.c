423535ae9bff1d8b4eb1f6174368bcb2a7c627a3
Vinicius Lopes <vilopes@c1r11s4.42porto.com>
2024-10-24 23:36:57 +0100

    update

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,19 +6,19 @@
 /*   By: vilopes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 22:49:57 by vilopes           #+#    #+#             */
-/*   Updated: 2024/10/24 23:32:16 by vilopes          ###   ########.fr       */
+/*   Updated: 2024/10/24 23:36:39 by vilopes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
 	size_t	len_dest;
 	size_t	len_src;
-	
+
 	len_dest = ft_strlen(dest);
 	len_src = ft_strlen(src);
 	if (size <= len_dest)
