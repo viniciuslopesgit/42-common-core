3210007749bc7742829e26ea9b350ba279a39864
Vinícius Lopes <contact.viniciuslopes@gmail.com>
2025-04-27 01:44:16 +0100

    exam-2

--- Exam42 /exam-2/ft_itoa/ft_itoa.c
+++ Exam42 /exam-2/ft_itoa/ft_itoa.c
@@ -5,25 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: viniciuslopes <viniciuslopes@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/20 18:38:53 by viniciuslop       #+#    #+#             */
-/*   Updated: 2025/04/26 23:12:15 by viniciuslop      ###   ########.fr       */
+/*   Created: 2025/04/27 01:26:14 by viniciuslop       #+#    #+#             */
+/*   Updated: 2025/04/27 01:43:36 by viniciuslop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <stdlib.h>
-#include <stdio.h>
 
-int ft_len_n(int nbr)
+int ft_len_nbr(int nbr)
 {
 	int len 	= 0;
-	
-	if (nbr <= 0)
+		
+	if (nbr == 0)
+		return (0);
+	if (nbr < 0)
 	{
 		len++;
 		nbr = nbr * -1;
 	}
-	while ( nbr > 0)
+	while (nbr > 0)
 	{
 		nbr = nbr / 10;
 		len++;
@@ -33,36 +34,24 @@ int ft_len_n(int nbr)
 
 char	*ft_itoa(int nbr)
 {
-	int len 	= ft_len_n(nbr);
-	char *str	= (char *)malloc(len + 1);
-	if (!str)
+	int len = ft_len_nbr(nbr);
+	char *result = (char *)malloc(sizeof(len) + 1);
+	if (!result)
 		return (NULL);
-	
-	str[len] = '\0';
+		
+	result[len] = '\0';
 	if (nbr == 0)
-		str[0] = '0';
+		result[0] = '0';
 	if (nbr < 0)
 	{
-		str[0] = '-';
+		result[0] = '-';
 		nbr = nbr * -1;
 	}
 	while (nbr > 0)
 	{
 		--len;
-		str[len] = (nbr % 10) + '0';
+		result[len] = (nbr % 10) + '0';
 		nbr = nbr / 10;
 	}
-	return (str);
-	
+	return (result);
 }
-
-// int main (int argc, char **argv)
-// {
-//      printf("%s\n", ft_itoa(0));             // "0"
-//      printf("%s\n", ft_itoa(42));            // "42"
-//      printf("%s\n", ft_itoa(-42));           // "-42"
-//      printf("%s\n", ft_itoa(2147483647));    // "2147483647"
-//      printf("%s\n", ft_itoa(-2147483647));   // "-2147483648"
-
-// 	 return (0);
-// }
